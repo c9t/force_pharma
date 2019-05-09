cf8b77dfb9377f9ed997a17c4f1ac9d82283f4bc
c9t <c9t@protonmail.com>
2019-05-09 19:02:13 +0300

    complete header. started to create about section

--- app/js/script.js
+++ app/js/script.js
@@ -5,8 +5,20 @@ $( document ).ready(function() {
     navigationBurger.on('click', function (e) {
         e.preventDefault();
         $(this).toggleClass('nav_add');
-
         headerMenu.toggleClass('open');
+    });
+//add class with scroll to 60px
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() > 60){
+
+            $('header').addClass("header_scroll");
+
+        }  else{
+
+            $('header').removeClass("header_scroll");
+        }
+
     });
 
         $(".testimonials_slider").owlCarousel({
